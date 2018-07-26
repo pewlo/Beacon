b26956295b3977f93c218a9fd8cd78bfdc52a2e5
Rafał <rafal.malczynski@gmail.com>
2018-07-26 11:07:05 +0200

    Exposed ObjC headers to Swift, so that we can use IndoorSDK

---
+++ Beacon/ObjCBridge.h
@@ -0,0 +1,15 @@
+//
+//  ObjCBridge.h
+//  Beacon
+//
+//  Created by user1 on 26/07/2018.
+//  Copyright © 2018 user1. All rights reserved.
+//
+
+#ifndef ObjCBridge_h
+#define ObjCBridge_h
+
+#import "EILIndoorSDK.h"
+#import <EstimoteSDK/EstimoteSDK.h>
+
+#endif /* ObjCBridge_h */
