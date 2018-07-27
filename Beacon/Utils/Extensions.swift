b470f79a83facd746717210445dda611d47a43cf
Rafał <rafal.malczynski@gmail.com>
2018-07-27 11:37:17 +0200

    Added custom MenuButton

---
+++ Beacon/Utils/Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Extensions.swift
+//  Beacon
+//
+//  Created by user1 on 27/07/2018.
+//  Copyright © 2018 user1. All rights reserved.
+//
+
+import Foundation
+
+extension UIColor {
+    @nonobjc class var bluePurple: UIColor {
+        return UIColor(red: 61.0 / 255.0, green: 14.0 / 255.0, blue: 238.0 / 255.0, alpha: 1.0)
+    }
+}
